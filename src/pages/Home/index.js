d041cc88ce34c61ad3f932c0743cad014b882965
nouredinemoussa <nouredine.moussa1@gmail.com>
2024-08-30 18:13:00 +0200

    Réglage de l'affichage du dernier évènement

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const { data } = useData();
+  const last = data ? data.events[data.events.length - 1] : null;
   return <>
     <header>
       <Menu />
@@ -116,13 +117,14 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
+        {last && (
+          <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
           label="boom"
-        />
+        />)}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
